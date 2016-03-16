19ae98c290dd894c1645b9c53df40659b51b6a7d
pawan.singh <pawan.singh@lbi.com>
2016-03-16 20:26:12 +0530

    initial commit

---
+++ WPF.SQLite/WPF.SQLite/ViewModel/StartViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using Microsoft.Win32;
+
+namespace WPF.SQLite.ViewModel
+{
+    public class StartViewModel : BaseViewModel
+    {
+        public ICommand CreateDbCommand { get; set; }
+
+        public StartViewModel()
+        {
+            CreateDbCommand = new RelayCommand(() =>
+            {
+                MessageBox.Show("Hello !");
+            });
+
+        }
+    }
+}
