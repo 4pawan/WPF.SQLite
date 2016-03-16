f36f65ed0d78bd85b143983240d7466cc13f972c
pawan.singh <pawan.singh@lbi.com>
2016-03-16 21:15:40 +0530

    initial commit

---
+++ WPF.SQLite/WPF.SQLite/EF/DbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.CodeFirst;
+
+namespace WPF.SQLite.EF
+{
+        public class MyDbContext : DbContext
+        {
+            public MyDbContext()
+                : base("ConnectionStringName")
+            { }
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                var sqliteConnectionInitializer = new SqliteCreateDatabaseIfNotExists<MyDbContext>(modelBuilder);
+                Database.SetInitializer(sqliteConnectionInitializer);
+            }
+        }
+   
+}
---
+++ WPF.SQLite/WPF.SQLite/EF/POCO/Model.cs
@@ -0,0 +1,36 @@
+using System.Data.Entity;
+
+namespace WPF.SQLite.EF.POCO
+{
+    public partial class Model : DbContext
+    {
+        public Model()
+            : base("name=DbContext")
+        {
+        }
+
+        public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
+        public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
+        public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
+        public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
+        public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AspNetRole>()
+                .HasMany(e => e.AspNetUsers)
+                .WithMany(e => e.AspNetRoles)
+                .Map(m => m.ToTable("AspNetUserRoles").MapLeftKey("RoleId").MapRightKey("UserId"));
+
+            modelBuilder.Entity<AspNetUser>()
+                .HasMany(e => e.AspNetUserClaims)
+                .WithRequired(e => e.AspNetUser)
+                .HasForeignKey(e => e.UserId);
+
+            modelBuilder.Entity<AspNetUser>()
+                .HasMany(e => e.AspNetUserLogins)
+                .WithRequired(e => e.AspNetUser)
+                .HasForeignKey(e => e.UserId);
+        }
+    }
+}
